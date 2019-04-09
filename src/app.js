e8fac7ac65fcdd36eaa12a47ba18da657c007d5c
acgyiyo <acgyiyo1@gmail.com>
2019-04-09 11:31:13 -0500

    *add routes for the views *add transations view panel *add summary panel *created profile view

--- src/app.js
+++ src/app.js
@@ -10,14 +10,30 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const accounData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 
-const accounts = JSON.parse(accounData);
+const accounts = JSON.parse(accountData);
 const users = JSON.parse(userData);
 
-app.get('/', (req, res) => res.render(
-    'index', { title: 'Account Summary', accounts }
-));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Account Summary', accounts });
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking })
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit })
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] });
+})
 
 app.listen(3000, () => console.log('Ps project running on port 3000!!'));
